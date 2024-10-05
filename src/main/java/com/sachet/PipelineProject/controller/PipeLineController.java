195dddf593337e440a770e7afbbc76e742f417e4
sachetwasti1997 <sachetwasti61@gmail.com>
2024-10-05 07:37:29 +0530

    Controller addition for main

---
+++ src/main/java/com/sachet/PipelineProject/controller/PipeLineController.java
@@ -0,0 +1,15 @@
+package com.sachet.PipelineProject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PipeLineController {
+
+    @GetMapping
+    public ResponseEntity<String> getHello() {
+        return ResponseEntity.ok("This is the main");
+    }
+
+}
