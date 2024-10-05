256e48852b97ab07dd07ed3a796800783edc2cf0
sachetwasti1997 <sachetwasti61@gmail.com>
2024-10-05 07:29:04 +0530

    Project Initialisation

---
+++ src/test/java/com/sachet/PipelineProject/PipelineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachet.PipelineProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
