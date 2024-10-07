dc574afd8f89db81adc208a9f7e61c274c357a66
sachetwasti1997 <sachetwasti61@gmail.com>
2024-10-07 18:26:42 +0530

    Code to sleep for a bit

--- src/main/java/com/sachet/PipelineProject/PipelineProjectApplication.java
+++ src/main/java/com/sachet/PipelineProject/PipelineProjectApplication.java
@@ -3,13 +3,20 @@ package com.sachet.PipelineProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class PipelineProjectApplication {
 
 	public static void main(String[] args) {
 		System.out.println("The project has started");
-		for (int i=0; i<10; i++) {
+		for (int i=0; i<50; i++) {
 			System.out.println("Printing the project start "+(i+1));
+			try {
+				Thread.sleep(1000);
+			}catch (InterruptedException e) {
+				System.out.println("Exception occured while sleeping");
+			}
 		}
 //		SpringApplication.run(PipelineProjectApplication.class, args);
 	}
